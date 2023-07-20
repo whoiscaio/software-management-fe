db11634e0384f16fada6f9116d4e20c6fc394114
whoiscaio <whoiscaio@protonmail.com>
2023-07-20 01:49:46 -0300

    feat: global styles with css reset provided

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,4 @@
-import { useEffect } from 'react';
-import { HttpClient } from './services/utils/HttpClient';
-
 function App() {
-  useEffect(() => {
-    (async () => {
-      const httpClient = new HttpClient();
-
-      await httpClient.get('ping');
-    })();
-  }, []);
-
   return (
     <>
       Software development system
---
+++ src/global/styles/GlobalStyles.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 16px;
+  }
+
+  button,
+  input,
+  textarea,
+  select {
+    outline: none;
+  }
+
+  button {
+    all: unset;
+
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyles;
