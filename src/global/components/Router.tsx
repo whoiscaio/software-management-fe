4e9e7ac20068afac2e494d3c3e57a46ba2dad90e
whoiscaio <whoiscaio@protonmail.com>
2023-07-20 13:01:50 -0300

    feat: automatic navigation to /login when the user is not authenticated yet

--- src/global/components/Router.tsx
+++ src/global/components/Router.tsx
@@ -1,7 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Auth from '../../pages/auth';
+import { useContext, useEffect } from 'react';
+import { AuthContext } from '../../contexts/authContext';
 
 export default function Router() {
+  const { jwt } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!jwt) {
+      navigate('/login');
+    }
+  }, [jwt, navigate]);
+
   return (
     <Routes>
       <Route path="/login" element={<Auth as="login" />} />
