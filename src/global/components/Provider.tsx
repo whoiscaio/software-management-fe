afef81cf862570b0a66d2e22c1f0409e08804612
whoiscaio <whoiscaio@protonmail.com>
2023-07-20 12:58:21 -0300

    feat: global provider component to handle context and general providers

---
+++ src/global/components/Provider.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import mainTheme from '../styles/theme/mainTheme';
+import GlobalStyles from '../styles/GlobalStyles';
+import AuthContextProvider from '../../contexts/authContext';
+
+type ProviderProps = {
+  children: ReactNode;
+};
+
+export default function Provider({ children }: ProviderProps) {
+  return (
+    <ThemeProvider theme={mainTheme}>
+      <GlobalStyles />
+      <AuthContextProvider>
+        <BrowserRouter>
+          {children}
+        </BrowserRouter>
+      </AuthContextProvider>
+    </ThemeProvider>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
 
 import App from './App.tsx';
-import GlobalStyles from './global/styles/GlobalStyles.ts';
-import mainTheme from './global/styles/theme/mainTheme.ts';
-import { BrowserRouter } from 'react-router-dom';
+import Provider from './global/components/Provider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={mainTheme}>
-      <GlobalStyles />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
