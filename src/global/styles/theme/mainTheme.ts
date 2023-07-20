33323ebcb136a15402aa9fe550baf43175b6f0bb
whoiscaio <whoiscaio@protonmail.com>
2023-07-20 13:09:36 -0300

    feat: global styled main theme colors

--- src/global/styles/theme/mainTheme.ts
+++ src/global/styles/theme/mainTheme.ts
@@ -1,4 +1,17 @@
-const mainTheme = {};
+const mainTheme = {
+  primary: {
+    taupe: '#463F3A',
+    battleshipGray: '#8A817C',
+    silver: '#BCB8B1',
+    isabelline: '#F4F3EE',
+    babyPowder: '#FFFEF7',
+    melon: '#E0AFA0',
+  },
+  secondary: {
+    darkLine: '#0E0E0E',
+    lightLine: '#F2F2E7',
+  }
+};
 
 export type Theme = typeof mainTheme;
 
