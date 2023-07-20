ff0eb2547a93f58ab37fe1306ba0e6f09c6a2029
whoiscaio <whoiscaio@protonmail.com>
2023-07-20 18:38:29 -0300

    feat: useFormErrors hook implemented

---
+++ src/hooks/useFormErrors.ts
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { IFormError } from '../types/formError';
+
+export default function useFormErrors() {
+  const [errors, setErrors] = useState<IFormError[]>([]);
+
+  function setError(field: string, message: string) {
+    if (errors.find((error) => error.field === field && error.message === message)) return;
+
+    cleanErrors(field);
+
+    setErrors((prevState) => [
+      ...prevState,
+      {
+        field,
+        message,
+      }
+    ]);
+  }
+
+  function cleanErrors(field: string) {
+    setErrors((prevState) => prevState.filter((error) => error.field !== field));
+  }
+
+  function getErrors(field: string) {
+    return errors.filter((error) => error.field === field);
+  }
+
+  return {
+    errors,
+    setError,
+    getErrors
+  };
+}
