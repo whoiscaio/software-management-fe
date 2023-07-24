d77ea5b82238bae950d2cb1e7c354a5f69d12926
whoiscaio <whoiscaio@protonmail.com>
2023-07-24 19:58:35 -0300

    feat: form modal added to create phase action

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,45 +1,62 @@
 import Page from '../../global/components/Page';
 import HomePageContainer from './styles';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { TeamContext } from '../../contexts/TeamContext';
 import { WorkspaceContext } from '../../contexts/WorkspaceContext';
 import PhaseList from './components/PhaseList';
 import { Plus } from 'lucide-react';
+import FormModal from '../../global/components/dialogs/FormModal';
 
 export default function Home() {
   const { selectedTeam } = useContext(TeamContext);
   const { selectedWorkspace } = useContext(WorkspaceContext);
 
-  function handleCreatePhase() {
+  const [isCreatePhaseFormOpen, setIsCreatePhaseFormOpen] = useState<boolean>(false);
+
+  async function handleCreatePhase(name: string, description?: string) {
     console.log('CREATE NEW PHASE');
+
+    setIsCreatePhaseFormOpen(false);
   }
 
   return (
-    <Page>
-      <HomePageContainer>
-        {
-          selectedTeam && selectedTeam.name && selectedWorkspace && selectedWorkspace.name ? (
-            <>
-              <header>
-                <div className="headings">
-                  <h1>Gerenciamento de processos</h1>
-                  <h2>{selectedTeam.name}</h2>
-                </div>
-                <div className="action">
-                  <button
-                    type="button"
-                    onClick={handleCreatePhase}
-                    className="contrast-button button-pattern-measures scale-down-hover-effect"
-                  ><Plus /> Nova Fase</button>
-                </div>
-              </header>
-              <PhaseList />
-            </>
-          ) : (
-            <p>Nenhum workspace foi selecionado</p>
-          )
-        }
-      </HomePageContainer>
-    </Page>
+    <>
+      <Page>
+        <HomePageContainer>
+          {
+            selectedTeam && selectedTeam.name && selectedWorkspace && selectedWorkspace.name ? (
+              <>
+                <header>
+                  <div className="headings">
+                    <h1>Gerenciamento de processos</h1>
+                    <h2>{selectedTeam.name}</h2>
+                  </div>
+                  <div className="action">
+                    <button
+                      type="button"
+                      onClick={() => setIsCreatePhaseFormOpen(true)}
+                      className="contrast-button button-pattern-measures scale-down-hover-effect"
+                    ><Plus /> Nova Fase</button>
+                  </div>
+                </header>
+                <PhaseList />
+              </>
+            ) : (
+              <p>Nenhum workspace foi selecionado</p>
+            )
+          }
+        </HomePageContainer>
+      </Page>
+      {
+        isCreatePhaseFormOpen && (
+          <FormModal
+            action={handleCreatePhase}
+            close={() => setIsCreatePhaseFormOpen(false)}
+            title="Criar fase"
+            confirmButtonText="Criar fase"
+          />
+        )
+      }
+    </>
   );
 }
