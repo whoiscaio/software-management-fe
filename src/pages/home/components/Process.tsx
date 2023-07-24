e53b218bf4f06f7c6aa65c593a9172ce51510700
whoiscaio <whoiscaio@protonmail.com>
2023-07-24 19:50:01 -0300

    feat: form modal added to process edit action

--- src/pages/home/components/Process.tsx
+++ src/pages/home/components/Process.tsx
@@ -4,6 +4,7 @@ import Subprocess from './Subprocess';
 import { MouseEvent, useMemo, useState } from 'react';
 import { ProcessContainer } from '../styles';
 import Dialog from '../../../global/components/dialogs/Dialog';
+import FormModal from '../../../global/components/dialogs/FormModal';
 
 type ProcessProps = {
   process: IProcess;
@@ -12,6 +13,7 @@ type ProcessProps = {
 export default function Process({ process }: ProcessProps) {
   const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState<boolean>(false);
   const [isSubprocessListOpen, setIsSubprocessListOpen] = useState<boolean>(false);
+  const [isEditFormOpen, setIsEditFormOpen] = useState<boolean>(false);
 
   function handleToggleSubprocessList() {
     if (!process.subprocesses || process.subprocesses.length < 1) return;
@@ -24,8 +26,15 @@ export default function Process({ process }: ProcessProps) {
     setIsDeleteDialogOpen(true);
   }
 
-  function handleEditProcess() {
+  function handleOpenEditForm(e: MouseEvent) {
+    e.stopPropagation();
+    setIsEditFormOpen(true);
+  }
+
+  async function handleEditProcess(name: string, description?: string) {
     console.log('HANDLE EDIT PROCESS');
+
+    setIsEditFormOpen(false);
   }
 
   function handleDeleteProcess() {
@@ -46,7 +55,7 @@ export default function Process({ process }: ProcessProps) {
           <p>{process.name}</p>
           <div className="actions">
             <button type="button" onClick={handleOpenDeleteDialog}><Trash color="#DD0000" size={25} /></button>
-            <button type="button" onClick={handleEditProcess}><Edit color="#0000BB" size={25} /></button>
+            <button type="button" onClick={handleOpenEditForm}><Edit color="#0000BB" size={25} /></button>
           </div>
         </div>
         {
@@ -71,6 +80,18 @@ export default function Process({ process }: ProcessProps) {
             confirmButtonText="Sim, deletar processo"
             close={() => setIsDeleteDialogOpen(false)}
             confirm={handleDeleteProcess}
+          />
+        )
+      }
+      {
+        isEditFormOpen && (
+          <FormModal
+            action={handleEditProcess}
+            close={() => setIsEditFormOpen(false)}
+            title="Editar processo"
+            confirmButtonText="Editar processo"
+            text1={process.name}
+            text2={process.description}
           />
         )
       }
